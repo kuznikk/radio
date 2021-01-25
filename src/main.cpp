72f3e5d3996b6550147ad877bf3b6f1cc635b5e8
kuznikk <45061763+kuznikk@users.noreply.github.com>
2021-01-25 09:56:21 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -14,19 +14,18 @@
 
 #include "ESPAsyncWebServer.h"
 #include "ESPAsyncTCP.h"
+#include "ESPAsyncWiFiManager.h"
 
 AsyncWebServer server(80);
+DNSServer dns;
 
 // pripojeni na wifi:
 boolean state = false;
 
-const char *SSID = "WLAN-456LB6";
-const char *PASSWORD = "1grfJRD9m5rB6J0";
-int stanice = 0;
+char *URL = "http://icecast3.play.cz/evropa2-128.mp3"; //Nastaveni stanice
+String stanice = "stanice";
 
-const char *URL = "http://icecast3.play.cz/evropa2-128.mp3"; //Nastaveni stanice
-const char *URL2 = "";
-const char *URL3 = "";
+
 
 
 AudioGeneratorMP3 *mp3;
@@ -65,72 +64,35 @@ void pressInterrupt() {
 void setup()
 {
   Serial.begin(115200);
-  //delay(1000);
-  Serial.println("Pripojovani k WiFi");
-  //attachInterrupt(digitalPinToInterrupt(1), pressInterrupt, RISING);//zapoznámkoval jsem to, házelo mi to errory 
 
-  /*WiFi.disconnect();
-  WiFi.softAPdisconnect(true);
-  WiFi.mode(WIFI_STA);*/
-
-  WiFi.begin(SSID, PASSWORD);
-
-  // Funkce se bude spouštět do zastavení
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.println("...pripojovani k WiFi");
-    delay(1000);
-  }
-  Serial.println("Pripojeno");
+  AsyncWiFiManager wifiManager(&server,&dns);
+  wifiManager.autoConnect("RadioAP");
+  Serial.println("connected");
+  Serial.println(WiFi.localIP());
 
   if(!SPIFFS.begin()){
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   } 
-  Serial.println(WiFi.localIP());
+  //Serial.println(WiFi.localIP());
 
 
    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SPIFFS, "/index.html", String());
+  }); 
+
+  server.on("/url", HTTP_POST, [](AsyncWebServerRequest *request) {    
+    stanice = request->arg("url").c_str();    
+    strcpy(URL, stanice.c_str());
+    request->send_P(200, "text/json", "{\"result\":\"ok\"}");
   });
 
-  server.on("/stanice0", HTTP_POST, [](AsyncWebServerRequest *request){
-    stanice = 0;  
-    request->send(200, "text/json", "{\"result\":\"ok\"}");
-  });
-
-  server.on("/stanice1", HTTP_POST, [](AsyncWebServerRequest *request){
-    stanice = 1;  
-    request->send(200, "text/json", "{\"result\":\"ok\"}");
-  });
-
-  server.on("/stanice2", HTTP_POST, [](AsyncWebServerRequest *request){
-    stanice = 2;  
-    request->send(200, "text/json", "{\"result\":\"ok\"}");
-  });
-
-  
-
- 
 
   audioLogger = &Serial;
-  switch(stanice){
-    case 0:
-     file = new AudioFileSourceICYStream(URL);
-     file->RegisterMetadataCB(MDCallback, (void*)"ICY");
-      break;
-    case 1:
-     file = new AudioFileSourceICYStream(URL2);
-     file->RegisterMetadataCB(MDCallback, (void*)"ICY");
-      break;
-    case 2:
-     file = new AudioFileSourceICYStream(URL3);
-     file->RegisterMetadataCB(MDCallback, (void*)"ICY");
-      break;
-
-   } 
 
 
-
+  file = new AudioFileSourceICYStream(URL);
+  file->RegisterMetadataCB(MDCallback, (void*)"ICY");
   buff = new AudioFileSourceBuffer(file, 2048);
   buff->RegisterStatusCB(StatusCallback, (void*)"buffer");
   out = new AudioOutputI2SNoDAC();
@@ -161,9 +123,9 @@ void loop()
       delay(1000);
     }
   }
+  Serial.println(URL);
+  Serial.println(stanice);
+  delay(2000);
 
- 
- Serial.println(stanice);
- delay(2000);
 
 }
